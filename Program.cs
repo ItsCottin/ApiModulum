d821891a86d001322cd4b06d89ba3ce37724c87d
ItsCottin <cottingfontes@hotmail.com>
2023-05-23 18:35:18 -0300

    Commit Inicial v2

--- Program.cs
+++ Program.cs
@@ -26,11 +26,11 @@ options.UseSqlServer(builder.Configuration.GetConnectionString("constring"));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseHttpsRedirection();
 
