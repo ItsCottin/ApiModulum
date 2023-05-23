a3f6b0484e1f8b153d31d30d5d2d7cc97760cd23
ItsCottin <cottingfontes@hotmail.com>
2023-05-23 20:12:43 -0300

    Commit ajuste swagger

--- Program.cs
+++ Program.cs
@@ -25,12 +25,9 @@ options.UseSqlServer(builder.Configuration.GetConnectionString("constring"));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-//}
+app.UseSwagger();
+
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
